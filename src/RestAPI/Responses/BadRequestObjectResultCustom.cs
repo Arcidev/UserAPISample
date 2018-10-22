7308f32a766dad3531e731cb7c9ae9b5f3cf91cc
Andrej Dobes <Arcidev@users.noreply.github.com>
2018-10-22 09:36:24 +0200

    BadRequestResult: slightly reworked BadRequestObjectResultCustom to have better output and added comments

--- src/RestAPI/Responses/BadRequestObjectResultCustom.cs
+++ src/RestAPI/Responses/BadRequestObjectResultCustom.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Linq;
 
 namespace RestAPI.Responses
 {
+    /// <summary>
+    /// Extends <see cref="BadRequestObjectResult"/> by providin custom error message
+    /// </summary>
     public class BadRequestObjectResultCustom : BadRequestObjectResult
     {
-        public BadRequestObjectResultCustom(ModelStateDictionary errors, string errorMessage) : base(new { Errors = errors?.Values.Select(x => x.Errors), Message = errorMessage })
+        /// <summary>
+        /// Creates new instance
+        /// </summary>
+        /// <param name="errors">Model state dictionary with errors</param>
+        /// <param name="errorMessage">Custom error message</param>
+        public BadRequestObjectResultCustom(ModelStateDictionary errors, string errorMessage) : base(errors)
         {
+            Value = new { Errors = Value, Message = errorMessage };
         }
     }
 }
